ece3bbf22da23635ee245ba5b912220cc1927268
Nishant-Pall <nishant@loopinhq.com>
2022-03-13 19:58:51 +0530

    Add relations b/w user and events

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,6 +23,17 @@ app.use('/graphql', graphqlHTTP({
 			date: String!
 		}
 
+		type User {
+			_id: ID!
+			email: String!
+			password: String
+		}
+
+		input UserType {
+			email: String!
+			password: String
+		}
+
 		input EventType {
 			title: String!
 			description: String!
@@ -34,6 +47,7 @@ app.use('/graphql', graphqlHTTP({
 
 		type rootMutation {
 			createEvent(eventInput: EventType): Event
+			createUser(userInput: UserType): User
 		}
 
 		schema {
@@ -46,7 +60,7 @@ app.use('/graphql', graphqlHTTP({
 			try {
 				const events = await Event.find();
 				return events.map(event => {
-					return { ...event._doc };
+					return { ...event._doc, _id: event.id };
 				});
 			} catch (err) {
 				throw err;
@@ -57,14 +71,41 @@ app.use('/graphql', graphqlHTTP({
 				title: args.eventInput.title,
 				description: args.eventInput.description,
 				price: +args.eventInput.price,
-				date: new Date(args.eventInput.date)
+				date: new Date(args.eventInput.date),
+				creator: '622dfcece05d89a92879d41c'
 			});
 			try {
-				const result = await event.save();
-				return { ...result._doc };
+				const createdEvent = await event.save();
+				const user = await User.findById('622dfcece05d89a92879d41c');
+				if (!user) {
+					throw new Error('User doesnt exist');
+				}
+				await user.createdEvents.push(event);
+				await user.save();
+				return createdEvent;
 
 			} catch (error) {
 				throw err;
+			}
+		},
+		createUser: async (args) => {
+
+			const existingUser = await User.findOne({ email: args.userInput.email });
+			if (existingUser) {
+				throw new Error('User exists already');
+			}
+			else {
+				try {
+					const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+					const user = new User({
+						email: args.userInput.email,
+						password: hashedPassword
+					});
+					const result = await user.save();
+					return { ...result._doc, _id: result.id, password: null };
+				} catch (err) {
+					throw err;
+				}
 			}
 		}
 	},
