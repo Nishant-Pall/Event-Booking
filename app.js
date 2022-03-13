d567562be9cdb4f80b5cd480fe32f73b267b5417
Nishant-Pall <nishant@loopinhq.com>
2022-03-13 14:07:08 +0530

    Setup graphql API

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.listen(3000, ()=>{
+app.use('/graphql', graphqlHttp({
+	schema: buildSchema(`
+
+		type rootQuery {
+			events: [String!]!
+		}
+
+		type rootMutation {
+			createEvent(name:String):String
+		}
+
+		schema {
+			query: rootQuery
+			mutation: rootMutation
+		}
+	`),
+	rootValue: {
+		events: () => {
+			return ['Coding', 'Gaming'];
+		},
+		createEvent: (args) => {
+			const eventName = args.name;
+			return eventName;
+		}
+	},
+	graphiql: true
+}));
+
+app.listen(3000, () => {
+	to;
 	console.log('SERVER LISTENING TO 3000');
-})
+});
